a02f0c4707b05e32ae08192dab98eb566aaaf149
bondarenkovita35-afk <bondarenkovita35@gmail.com>
2025-10-18 12:35:09 +0200

    Add project files.

---
+++ WpfAppProekt/Models/Product.cs
@@ -0,0 +1,9 @@
+﻿
+namespace WpfAppProekt.Models;
+
+public class Product
+{
+    public string Name { get; set; } = null!;
+    public decimal Price { get; set; } 
+
+}
---
+++ WpfAppProekt/ViewModels/RelayCommand.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace WpfAppProekt.ViewModels
+{
+    public sealed class RelayCommand : ICommand
+    {
+        public event EventHandler? CanExecuteChanged;
+
+        private readonly Action<object?> _execute;
+        private readonly Func<object?, bool>? _can;
+
+        public RelayCommand(Action<object?> execute, Func<object?, bool>? can = null)
+        {
+            _execute = execute ?? throw new ArgumentNullException(nameof(execute));
+            _can = can;
+        }
+
+        public bool CanExecute(object? parameter)
+        {
+            return _can?.Invoke(parameter) ?? true;
+        }
+
+        public void Execute(object? parameter)
+        {
+            _execute(parameter);
+        }
+    }
+}
